f40b7cdb59448fa86d5a767590356ce793c59180
jonataoliveira93 <jonataoliveiradasilva1993@outlook.com>
2023-03-24 20:16:27 -0300

    projeto

--- exercicios.js
+++ exercicios.js
@@ -17,77 +17,115 @@ function imprimeMensagem() {
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
+function calculaAreaRetangulo(altura, largura) {
+let altura1 = prompt`qual a altura do retangulo?`
+let largura1 = prompt`qual a largura do retangulo?`
+let area = altura1 * largura1
+ 
+ console.log(area) 
 
 }
 
+
 // EXERCÍCIO 02
 function imprimeIdade() {
-  // implemente sua lógica aqui
+  let anoAtual = prompt(`qual ano nós estamos?`)
+  let anoNascimento = prompt(`que ano você nasceu?`)
+  let idade = anoAtual - anoNascimento
+  
+  console.log(idade)
+
+ 
 
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
-
+  let imc = peso/(altura*altura)
+  
+return imc
 }
+console.log(calculaIMC)
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+let nome = prompt (`qual seu nome?`)
+let idade = Number (prompt(`qual sua idade?`))
+let email = prompt (`qual seu email?`)
+  
+console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
 }
 
+
 // EXERCÍCIO 05
-function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
+function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
+  
+  let primeiraPergunta = prompt("qual sua primeira cor favorita?")
+  let segundaPergunta = prompt("qual sua segunda cor favorita?")
+  let terceiraPergunta = prompt("qual sua terceira cor favorita?")
+
+  arrayCores = [cor1, cor2, cor3]
+
+console.log(arrayCores)
 
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
+
+  return string.toUpperCase()
 
 }
+console.log(retornaStringEmMaiuscula)
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+   
+
+  return custo/valorIngresso
 
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+  
+ return string1.length === string2.length
 
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
+array[array.length-1]
+
+
 
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
-  // implemente sua lógica aqui
+ const ultimo = array.splice
 
+ return ultimo
 }
+
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  // implemente sua lógica aqui
+const troca = array.splice(array.length-1,1,array[0])[0]
+
+array[0] = troca
+
+return array
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
+  return string1.toLowerCase() === string2.toLowerCase
 
-}
+  console.log(checaIgualdadeDesconsiderandoCase(`olá`,`olA`))
+} 
+
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
